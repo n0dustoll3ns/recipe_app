8a1f6f3dcb57a48af9e17ba5ec22877bb3be8d72
n0dustoll3ns <fominykh.dg@gmail.com>
2022-10-10 20:20:30 +0300

    meal tile elements

--- lib/presentation/screens/common/meal_tile.dart
+++ lib/presentation/screens/common/meal_tile.dart
@@ -1,4 +1,6 @@
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:recipe_app/platform/size_config.dart';
 
 import '../../../model/recipe.dart';
@@ -16,7 +18,63 @@ class MealTile extends StatelessWidget {
           borderRadius: BorderRadius.circular(10.0),
           child: Stack(
             fit: StackFit.expand,
-            children: [Image.asset(recipe.image)],
+            children: [
+              Image.asset(recipe.image),
+              const DecoratedBox(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: FractionalOffset.topCenter,
+                        end: FractionalOffset.bottomCenter,
+                        colors: [Colors.transparent, Colors.black],
+                        stops: [0.0, 1.0])),
+              ),
+              Padding(
+                padding: EdgeInsets.all(SizeConfig.blockSizeHorizontal * 4),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: DecoratedBox(
+                        decoration: const BoxDecoration(
+                            color: Color(0xffFFE1B3), borderRadius: BorderRadius.all(Radius.circular(20))),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Transform.scale(
+                              scale: 0.7,
+                              child: Icon(Icons.star, color: Color(0xFFFFAD30)),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(right: SizeConfig.blockSizeHorizontal * 2.4),
+                              child: Text(
+                                '${recipe.rating}',
+                                style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.w400, fontSize: 11, color: Color(0xff000000)),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      '${recipe.name}',
+                      maxLines: 2,
+                      style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w600, fontSize: 11, color: Color(0xffFFFFFF)),
+                    ),
+                    SizedBox(height: SizeConfig.blockSizeHorizontal * 1.5),
+                    Text(
+                      'By ${recipe.creatorName}',
+                      maxLines: 1,
+                      style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w600, fontSize: 8, color: Color(0xffA9A9A9)),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
