748a51eb30fe0153cef18f4c0d9339b439d882e2
n0dustoll3ns <fominykh.dg@gmail.com>
2022-10-12 22:19:34 +0300

    oop extending for recipe and author models Food and ingridient models

---
+++ lib/presentation/screens/common/ingridient_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../../../model/ingridient.dart';
+
+class IngridientCard extends StatelessWidget {
+  final Ingridient ingridient;
+  const IngridientCard({super.key, required this.ingridient});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
